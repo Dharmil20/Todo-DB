239fd6955d4aefebf1ad3d0a43e38370def88628
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-06 08:18:58 +0000

    Add Authentication

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 const JWT_SECRET = "randomdharmillovescoding";
 const app = express();
 
+mongoose.connect(
+  "mongodb+srv://dharmiltrivedi5:4BTC5fjyuZX1zCjj@cluster0.nqozu.mongodb.net/todo-app-database"
+);
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+  try {
+    await UserModel.create({
+      email: email,
+      password: password,
+      name: name,
+    });
 
-  await UserModel.insert({
-    email: email,
-    password: password,
-    name: name,
-  });
-
-  res.json({
-    message: "You are Signed Up!!",
-  });
+    res.json({
+      message: "You are Signed Up!!",
+    });
+  } catch {
+    res.json({
+      message: "User Already signed up",
+    });
+  }
 });
 
 app.post("/login", (req, res) => {
@@ -32,7 +41,15 @@ app.post("/login", (req, res) => {
   });
 
   if (user) {
-    const token = jwt.sign();
+    const token = jwt.sign(
+      {
+        id: user._id.toString()
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
   } else {
     res.status(403).json({
       message: "Invalid Credentials",
@@ -41,15 +58,21 @@ app.post("/login", (req, res) => {
 });
 
 function auth(req, res, next) {
-  //code
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token,JWT_SECRET);
+
+  console.log(decodedData);
+  next();
 }
 
 app.post("/todo", auth, (req, res) => {
-  //code
+  res.json({
+    message: "Your todos"
+  })
 });
 
 app.get("/todos", auth, (req, res) => {
-  //code
+  //
 });
 
 app.listen(3000);
